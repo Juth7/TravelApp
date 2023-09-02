2d5b1505f6650e41a6820490c74566d69fe593f5
Judith Goncalves <87342173+Juth7@users.noreply.github.com>
2023-09-02 01:06:07 -0400

    Update mongo.js

--- connections/mongo.js
+++ connections/mongo.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-mongoose.set('strictQuery', false);
+// mongoose.set('strictQuery', false);
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
